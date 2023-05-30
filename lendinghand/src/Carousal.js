33cc9ce1cf1f2f085732451c5cd1c5db3741c757
princeyadav00785 <princeyadav00785@example.com>
2023-05-30 14:30:02 +0530

    front end improvement

--- lendinghand/src/Carousal.js
+++ lendinghand/src/Carousal.js
@@ -1,41 +1,38 @@
-import React from "react";
-import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+import React, { useState } from "react";
+import "./Carousal.css";
 import img1 from "./img1.png";
 import img2 from "./img2.png";
 import img3 from "./img3.png";
-export default function App() {
+
+const Carousel = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [img2, img3, img1];
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+    );
+  };
+
   return (
-    <MDBCarousel showIndicators showControls fade>
-      <div className="caraousel">
-        <MDBCarouselItem
-          className="w-100 d-block  "
-          itemId={1}
-          src={img2}
-          alt="..."
-        >
-          {/* <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-        </MDBCarouselItem>
-
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={2}
-          src={img3}
-          alt="..."
-        >
-          {/* <h5>Second slide label</h5> */}
-        </MDBCarouselItem>
-
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={3}
-          src={img1}
-          alt="..."
-        >
-          {/* <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
-        </MDBCarouselItem>
-      </div>
-    </MDBCarousel>
+    <div className="carousel">
+      <button className="carousel__button" onClick={prevSlide}>
+        &lt;
+      </button>
+      <img
+        className="carousel__image"
+        src={slides[currentSlide]}
+        alt={`Slide ${currentSlide + 1}`}
+      />
+      <button className="carousel__button" onClick={nextSlide}>
+        &gt;
+      </button>
+    </div>
   );
-}
+};
+
+export default Carousel;
